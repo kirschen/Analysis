b7ba519d8c54ece95250761a454c37460e865749
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-02-14 14:57:26 +0100

    update first setup

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -4,12 +4,7 @@
 '''
 import os
 
-if os.environ['USER'] in ['llechner']:
-    plot_directory                      = "/afs/hephy.at/user/l/llechner/www/TTGammaEFT/"
-    cache_directory                     = "/afs/hephy.at/data/llechner01/TTGammaEFT/cache/"
-else:
-    ## This definition might not work for everybody. Update with whatever you like.
-    plot_directory                      = "/afs/hephy.at/user/%s/%s/www/Analysis/"%(os.environ['USER'][0],os.environ['USER'])
-    cache_directory                     = "/afs/hephy.at/data/%s01/Analysis/"%os.environ['USER']
+plot_directory                      = "/afs/hephy.at/user/%s/%s/www/Analysis/"%(os.environ['USER'][0],os.environ['USER'])
+cache_directory                     = "/afs/hephy.at/data/%s01/Analysis/"%os.environ['USER']
 
 if not os.path.isdir(cache_directory): os.makedirs(cache_directory)
