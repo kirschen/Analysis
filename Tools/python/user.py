53fcb623729278528d2b4d81caaf4b908a5c67c8
Lukas Lechner <llechner@users.noreply.github.com>
2019-02-18 12:15:08 +0100

    update user information
    also needed for condor submission

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -1,10 +1,12 @@
-''' Some definitions.
-    To avoid changes of the file being pushed to github, e.g. when you define a different dbDir, use:
-    git update-index --skip-worktree config.py
+'''
+Some definitions.
 '''
 import os
-
-plot_directory                      = "/afs/hephy.at/user/%s/%s/www/Analysis/"%(os.environ['USER'][0],os.environ['USER'])
-cache_directory                     = "/afs/hephy.at/data/%s01/Analysis/"%os.environ['USER']
+if os.environ['USER'] in ['llechner']:
+    plot_directory  = "/afs/hephy.at/user/l/llechner01/www/TTGammaEFT/"
+    cache_directory = "/afs/hephy.at/data/llechner01/TTGammaEFT/cache/"
+else:
+    plot_directory  = "/afs/hephy.at/user/%s/%s/www/Analysis/"%(os.environ['USER'][0],os.environ['USER'])
+    cache_directory = "/afs/hephy.at/data/%s01/Analysis/"%os.environ['USER']
 
 if not os.path.isdir(cache_directory): os.makedirs(cache_directory)
