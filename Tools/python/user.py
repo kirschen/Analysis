6071a577031b52122c3bc53fc4ff7e2a66b07a50
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2019-02-22 11:39:43 +0100

    adding dspitzbart

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -3,8 +3,11 @@ Some definitions.
 '''
 import os
 if os.environ['USER'] in ['llechner']:
-    plot_directory  = "/afs/hephy.at/user/l/llechner01/www/TTGammaEFT/"
+    plot_directory  = "/afs/hephy.at/user/l/llechner/www/TTGammaEFT/"
     cache_directory = "/afs/hephy.at/data/llechner01/TTGammaEFT/cache/"
+elif os.environ['USER'] in ['dspitzba', 'dspitzbart']:
+    plot_directory  = "/afs/hephy.at/user/d/dspitzbart/www/stopsDileptonLegacy/"
+    cache_directory = "/afs/hephy.at/data/dspitzbart01/cache/"
 else:
     plot_directory  = "/afs/hephy.at/user/%s/%s/www/Analysis/"%(os.environ['USER'][0],os.environ['USER'])
     cache_directory = "/afs/hephy.at/data/%s01/Analysis/"%os.environ['USER']
