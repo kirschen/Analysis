a735b3c926204289593eec26fb4678efa3e9e2db
Lukas LechnerÃ <lukas.k.lechner@gmail.com>
2019-02-22 12:29:53 +0100

    update user info

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -3,13 +3,16 @@ Some definitions.
 '''
 import os
 if os.environ['USER'] in ['llechner']:
-    plot_directory  = "/afs/hephy.at/user/l/llechner/www/TTGammaEFT/"
-    cache_directory = "/afs/hephy.at/data/llechner01/TTGammaEFT/cache/"
+    plot_directory         = "/afs/hephy.at/user/l/llechner/www/TTGammaEFT/"
+    cache_directory        = "/afs/hephy.at/data/llechner01/TTGammaEFT/cache/"
+    cern_proxy_certificate = "/afs/cern.ch/user/l/llechner/private/.proxy"
 elif os.environ['USER'] in ['dspitzba', 'dspitzbart']:
-    plot_directory  = "/afs/hephy.at/user/d/dspitzbart/www/stopsDileptonLegacy/"
-    cache_directory = "/afs/hephy.at/data/dspitzbart01/cache/"
+    plot_directory         = "/afs/hephy.at/user/d/dspitzbart/www/stopsDileptonLegacy/"
+    cache_directory        = "/afs/hephy.at/data/dspitzbart01/cache/"
+    cern_proxy_certificate = "/afs/cern.ch/user/d/dspitzba/private/.proxy"
 else:
-    plot_directory  = "/afs/hephy.at/user/%s/%s/www/Analysis/"%(os.environ['USER'][0],os.environ['USER'])
-    cache_directory = "/afs/hephy.at/data/%s01/Analysis/"%os.environ['USER']
+    plot_directory         = "/afs/hephy.at/user/%s/%s/www/Analysis/"%(os.environ['USER'][0],os.environ['USER'])
+    cache_directory        = "/afs/hephy.at/data/%s01/Analysis/"%os.environ['USER']
+    cern_proxy_certificate = "/afs/cern.ch/user/%s/%s/private/.proxy"%(os.environ['USER'][0],os.environ['USER'])
 
 if not os.path.isdir(cache_directory): os.makedirs(cache_directory)
