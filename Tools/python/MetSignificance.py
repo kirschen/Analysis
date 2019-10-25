222d5b5ea71a31e9825e95364fc12e60ebdbf25c
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-10-25 18:48:33 +0200

    fix MetSig issue

--- Tools/python/MetSignificance.py
+++ Tools/python/MetSignificance.py
@@ -51,47 +51,37 @@ class MetSignificance:
         self.files            = [ f for f in sample.files if nonEmptyFile(f) ]
         self.outfiles         = None
 
-        if year == 2016:
+        if self.year == 2016:
             metSigParamsMC      = [1.617529475909303, 1.617529475909303, 1.4505983036866312, 1.4505983036866312, 1.411498565372343, 1.411498565372343, 1.4087559908291813, 1.4087559908291813, 1.3633674107893856, 1.3633674107893856, 0.0019861227075085516, 0.6539410816436597]
             metSigParamsData    = [1.843242937068234, 1.843242937068234, 1.64107911184195,   1.64107911184195,   1.567040591823117, 1.567040591823117, 1.5077143780804294, 1.5077143780804294, 1.614014783345394,  1.614014783345394, -0.0005986196920895609, 0.6071479349467596]
-            JER                 = "Summer16_25nsV1_MC" if not self.isData else "Summer16_25nsV1_DATA"
-            archive             = '' if not sample.isData else "Summer16_07Aug2017_V11_DATA"
-            jetThreshold = 15
+            self.JER                 = "Summer16_25nsV1_MC" if not self.isData else "Summer16_25nsV1_DATA"
+            self.jetThreshold = 15
 
-        elif year == 2017:
+        elif self.year == 2017:
             metSigParamsMC      = [1.9648214119268503, 1.5343086462230238, 1.9167197601498538, 1.5145044341064964, 1.8069380221985405, 1.3217263662622654, 1.5506294867561126, 1.272977540964842, 1.50742322311234,   1.6542883449796797, -0.0017865650107230548,  0.6593106706741719]
             metSigParamsData    = [2.228118299837604,  1.2420725475347338, 2.227630982417529,  1.256752205787215,  2.0215250734187853, 1.1557507029911258, 1.7350536144535336, 1.1587692458345757, 1.9385081854607988, 1.8726188460472792, -2.6697894266706265e-05, 0.646984812801919]
-            JER                 = "Fall17_V3_MC" if not self.isData else "Fall17_V3_DATA"
-            archive             = '' if not sample.isData else  "Fall17_17Nov2017_V32_DATA"
-            jetThreshold = 15
+            self.JER                 = "Fall17_V3_MC" if not self.isData else "Fall17_V3_DATA"
+            self.jetThreshold = 15
 
-        elif year == 2018:
+        elif self.year == 2018:
             metSigParamsMC      = [1.8430848616315363, 1.8430848616315363, 1.8572853766660877, 1.8572853766660877, 1.613083160233781,  1.613083160233781,  1.3966398718198898, 1.3966398718198898, 1.4831008506492056, 1.4831008506492056, 0.0011310724285762122, 0.6929410058142578]
             metSigParamsData    = [1.6231076732985186, 1.6231076732985186, 1.615595174619551,  1.615595174619551,  1.4731794897915416, 1.4731794897915416, 1.5183631493937553, 1.5183631493937553, 2.145670387603659,  2.145670387603659, -0.0001524158603362826, 0.7510574688006575]
-            JER                 = "Autumn18_V1_MC" if not self.isData else "Autumn18_V1_DATA"
-            archive             = '' if not sample.isData else "Autumn18_V19_DATA"
-            jetThreshold = 25
+            self.JER                 = "Autumn18_V1_MC" if not self.isData else "Autumn18_V1_DATA"
+            self.jetThreshold = 25
 
 
         self.era = None
         if self.isData:
             self.era = extractEra(self.name)[-1]
 
-        logger.info("Using JERs: %s", JER)
+        logger.info("Using JERs: %s", self.JER)
 
-        unclEnThreshold = 15
-        vetoEtaRegion   = (2.65, 3.14) if year == 2017 else (10,10)
-        METCollection   = "METFixEE2017" if year == 2017 else "MET"
+        self.unclEnThreshold = 15
+        self.vetoEtaRegion   = (2.65, 3.14) if self.year == 2017 else (10,10)
+        self.METCollection   = "METFixEE2017" if self.year == 2017 else "MET"
 
         # set the params for MET Significance calculation
         self.metSigParams = metSigParamsMC if not self.isData else metSigParamsData
-
-        self.modules = []
-    
-        JMECorrector = createJMECorrector( isMC=(not self.isData), dataYear=self.year, runPeriod=self.era, jesUncert="Total", jetType = "AK4PFchs", metBranchName=METCollection, applySmearing=False )
-        self.modules.append( JMECorrector() )
-
-        self.modules.append( METSigProducer(JER, self.metSigParams, METCollection=METCollection, useRecorr=True, calcVariations=(not self.isData), jetThreshold=jetThreshold, vetoEtaRegion=vetoEtaRegion) )
 
     def __call__( self, cut ):
         newFileList = []
@@ -99,6 +89,13 @@ class MetSignificance:
         for f in self.files:
             # need a hash to avoid data loss
             file_hash = str(hash(f))
+
+            self.modules = []
+    
+            JMECorrector = createJMECorrector( isMC=(not self.isData), dataYear=self.year, runPeriod=self.era, jesUncert="Total", jetType = "AK4PFchs", metBranchName=self.METCollection, applySmearing=False )
+            self.modules.append( JMECorrector() )
+            self.modules.append( METSigProducer(self.JER, self.metSigParams, METCollection=self.METCollection, useRecorr=True, calcVariations=(not self.isData), jetThreshold=self.jetThreshold, vetoEtaRegion=self.vetoEtaRegion) )
+
             p = PostProcessor( self.output_directory, [f], cut=cut, modules=self.modules, postfix="%s_%s"%(self.postfix, file_hash))
             p.run()
             newFileList += [self.output_directory + '/' + f.split('/')[-1].replace('.root', '%s_%s.root'%(self.postfix, file_hash))]
