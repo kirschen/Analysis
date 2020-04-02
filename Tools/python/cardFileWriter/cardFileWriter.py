569eabb7d5d13876cb6c309d80fc89b42e0b973e
Lukas Lechner <lukas.k.lechner@gmail.com>
2020-04-02 14:52:27 +0200

    adding readNLL to cardfilewriter

--- Tools/python/cardFileWriter/cardFileWriter.py
+++ Tools/python/cardFileWriter/cardFileWriter.py
@@ -416,6 +416,21 @@ class cardFileWriter:
         shutil.rmtree(uniqueDirname)
         return res
 
+    def readNLLFile(self, fname):
+        import ROOT
+        f = ROOT.TFile.Open(fname)
+        t = f.Get("limit")
+        nll = {}
+        t.GetEntry(0) # changed from 1!
+        # prefit NLL
+        nll["nll0"] = t.nll0
+        # delta NLL to prefit (should always be negative since stuff is fitted)
+        nll["nll"] = t.nll
+        # absolute NLL postfit
+        nll["nll_abs"] = t.nll0 + t.nll
+        f.Close()
+        return nll
+
     def calcNLL(self, fname=None, options=""):
         '''
         Does max likelihood fits, both with r=1 and a best-fit value
