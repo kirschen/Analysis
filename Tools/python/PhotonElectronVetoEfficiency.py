4474794ad2b341184d3ebe19c37324bb726e2148
Lukas Lechner <lukas.k.lechner@gmail.com>
2020-06-18 12:06:48 +0200

    update photon veto efficiency

--- Tools/python/PhotonElectronVetoEfficiency.py
+++ Tools/python/PhotonElectronVetoEfficiency.py
@@ -11,13 +11,14 @@ logger = logging.getLogger(__name__)
 g16_keys = [( "g2016_ScalingFactors_80X_Summer16.root", "Scaling_Factors_CSEV_R9 Inclusive"   ),
             ( "g2016_ScalingFactors_80X_Summer16.root", "Scaling_Factors_HasPix_R9 Inclusive" )]
 
-# UPDATE 2017 WHEN AVAILABLE, for all years the same?
-g17_keys = [( "g2016_ScalingFactors_80X_Summer16.root", "Scaling_Factors_CSEV_R9 Inclusive"   ),
-            ( "g2016_ScalingFactors_80X_Summer16.root", "Scaling_Factors_HasPix_R9 Inclusive" )]
+g17_keys = [( "g2017_CSEV_ScaleFactors_2017.root",      "Medium_ID"   ),
+            ( "g2017_PixelSeed_ScaleFactors_2017.root", "Medium_ID" )]
 
-# UPDATE 2018 WHEN AVAILABLE, for all years the same?
-g18_keys = [( "g2016_ScalingFactors_80X_Summer16.root", "Scaling_Factors_CSEV_R9 Inclusive"   ),
-            ( "g2016_ScalingFactors_80X_Summer16.root", "Scaling_Factors_HasPix_R9 Inclusive" )]
+g18_keys = [( "g2018_HasPix_2018.root", "eleVeto_SF"   ),
+            ( "g2018_CSEV_2018.root",   "eleVeto_SF" )]
+
+g18_keys_unc = [( "g2018_HasPix_2018.root", "eleVeto_Unc"   ),
+                ( "g2018_CSEV_2018.root",   "eleVeto_Unc" )]
 
 class PhotonElectronVetoEfficiency:
     def __init__( self, year ):
@@ -25,21 +26,39 @@ class PhotonElectronVetoEfficiency:
         if year not in [ 2016, 2017, 2018 ]:
             raise Exception("Photon Veto SF for year %i not known"%year)
 
-        if year == 2016:
+        self.year = year
+
+        if self.year == 2016:
             g_keys = g16_keys
-        elif year == 2017:
+        elif self.year == 2017:
             g_keys = g17_keys
-        elif year == 2018:
-            g_keys = g18_keys
+        elif self.year == 2018:
+            g_keys     = g18_keys
+            g_keys_unc = g18_keys_unc
 
         self.dataDir = "$CMSSW_BASE/src/Analysis/Tools/data/photonSFData"
         self.g_sf = [ getObjFromFile(os.path.expandvars(os.path.join(self.dataDir, file)), key) for (file, key) in g_keys ]
+        if self.year == 2018:
+            self.g_sf_unc = [ getObjFromFile(os.path.expandvars(os.path.join(self.dataDir, file)), key) for (file, key) in g_keys_unc ]
+            for effMap in self.g_sf_unc: assert effMap
 
         for effMap in self.g_sf: assert effMap
 
-    def getPartialSF( self, effMap, pt, eta ):
-        sf  = effMap.GetBinContent( effMap.FindBin(eta, pt) )
-        err = effMap.GetBinError(   effMap.FindBin(eta, pt) )
+    def getPartialSF( self, effMap, pt, eta, effMap_unc=None ):
+        if self.year == 2016:
+            bin = effMap.FindBin(eta,pt)
+        elif self.year == 2017:
+            bin = 1 if abs(eta) < 1.479 else 4
+        elif self.year == 2018:
+            bin = effMap.FindBin(pt,eta)
+
+        sf  = effMap.GetBinContent( bin )
+
+        if self.year == 2018:
+            err = effMap_unc.GetBinContent( bin )
+        else:
+            err = effMap.GetBinError( bin )
+
         return u_float(sf, err)
 
     def mult( self, list ):
@@ -50,14 +69,17 @@ class PhotonElectronVetoEfficiency:
     def getSF( self, pt, eta, sigma=0 ):
         if pt       >= 200: pt  = 199
         if abs(eta) >= 2.5: eta = 2.49 
-        sf = self.mult( [ self.getPartialSF( effMap, pt, abs(eta) ) for effMap in self.g_sf ] )
+        if self.year == 2018:
+            sf = self.mult( [ self.getPartialSF( effMap, pt, abs(eta), self.g_sf_unc[i] ) for i, effMap in enumerate(self.g_sf) ] )
+        else:
+            sf = self.mult( [ self.getPartialSF( effMap, pt, abs(eta) ) for effMap in self.g_sf ] )
 
         return (1+sf.sigma*sigma)*sf.val
 
 
 if __name__ == "__main__":
 
-    sigma = -1
+    sigma = 0
     print "2016"
     LSF = PhotonElectronVetoEfficiency(year=2016)
     print LSF.getSF(20, 1, sigma=sigma)
@@ -70,15 +92,15 @@ if __name__ == "__main__":
     print LSF.getSF(200, 1, sigma=sigma)
     print LSF.getSF(200, -1, sigma=sigma)
 
-    print LSF.getSF(20, 2.5, sigma=sigma)
-    print LSF.getSF(20, -2.5, sigma=sigma)
-    print LSF.getSF(20, 2.5, sigma=sigma)
-    print LSF.getSF(20, -2.5, sigma=sigma)
+    print LSF.getSF(20, 1.4, sigma=sigma)
+    print LSF.getSF(20, -1.4, sigma=sigma)
+    print LSF.getSF(20, 1.4, sigma=sigma)
+    print LSF.getSF(20, -1.4, sigma=sigma)
 
-    print LSF.getSF(200, 2.5, sigma=sigma)
-    print LSF.getSF(200, -2.5, sigma=sigma)
-    print LSF.getSF(200, 2.5, sigma=sigma)
-    print LSF.getSF(200, -2.5, sigma=sigma)
+    print LSF.getSF(200, 1.4, sigma=sigma)
+    print LSF.getSF(200, -1.4, sigma=sigma)
+    print LSF.getSF(200, 1.4, sigma=sigma)
+    print LSF.getSF(200, -1.4, sigma=sigma)
 
     print "2017"
     LSF = PhotonElectronVetoEfficiency(year=2017)
@@ -92,15 +114,15 @@ if __name__ == "__main__":
     print LSF.getSF(200, 1, sigma=sigma)
     print LSF.getSF(200, -1, sigma=sigma)
 
-    print LSF.getSF(20, 2.5, sigma=sigma)
-    print LSF.getSF(20, -2.5, sigma=sigma)
-    print LSF.getSF(20, 2.5, sigma=sigma)
-    print LSF.getSF(20, -2.5, sigma=sigma)
+    print LSF.getSF(20, 1.4, sigma=sigma)
+    print LSF.getSF(20, -1.4, sigma=sigma)
+    print LSF.getSF(20, 1.4, sigma=sigma)
+    print LSF.getSF(20, -1.4, sigma=sigma)
 
-    print LSF.getSF(200, 2.5, sigma=sigma)
-    print LSF.getSF(200, -2.5, sigma=sigma)
-    print LSF.getSF(200, 2.5, sigma=sigma)
-    print LSF.getSF(200, -2.5, sigma=sigma)
+    print LSF.getSF(200, 1.4, sigma=sigma)
+    print LSF.getSF(200, -1.4, sigma=sigma)
+    print LSF.getSF(200, 1.4, sigma=sigma)
+    print LSF.getSF(200, -1.4, sigma=sigma)
 
     print "2018"
     LSF = PhotonElectronVetoEfficiency(year=2018)
@@ -114,13 +136,13 @@ if __name__ == "__main__":
     print LSF.getSF(200, 1, sigma=sigma)
     print LSF.getSF(200, -1, sigma=sigma)
 
-    print LSF.getSF(20, 2.5, sigma=sigma)
-    print LSF.getSF(20, -2.5, sigma=sigma)
-    print LSF.getSF(20, 2.5, sigma=sigma)
-    print LSF.getSF(20, -2.5, sigma=sigma)
+    print LSF.getSF(20, 1.4, sigma=sigma)
+    print LSF.getSF(20, -1.4, sigma=sigma)
+    print LSF.getSF(20, 1.4, sigma=sigma)
+    print LSF.getSF(20, -1.4, sigma=sigma)
 
-    print LSF.getSF(200, 2.5, sigma=sigma)
-    print LSF.getSF(200, -2.5, sigma=sigma)
-    print LSF.getSF(200, 2.5, sigma=sigma)
-    print LSF.getSF(200, -2.5, sigma=sigma)
+    print LSF.getSF(200, 1.4, sigma=sigma)
+    print LSF.getSF(200, -1.4, sigma=sigma)
+    print LSF.getSF(200, 1.4, sigma=sigma)
+    print LSF.getSF(200, -1.4, sigma=sigma)
 
